b4a7e024ecaa36effbdabfe734ac668e1a625fa9
Ryan Zidago <ryanzidago@gmail.com>
2020-04-21 18:51:36 +0200

    linked all processes together

--- lib/todo/database.ex
+++ lib/todo/database.ex
@@ -3,9 +3,9 @@ defmodule Todo.Database do
 
   @db_folder "./persist"
 
-  def start do
+  def start_link do
     IO.puts("Starting the database server.")
-    GenServer.start(__MODULE__, nil, name: __MODULE__)
+    GenServer.start_link(__MODULE__, nil, name: __MODULE__)
   end
 
   def store(key, data) do
@@ -38,7 +38,7 @@ defmodule Todo.Database do
 
   defp start_workers do
     for index <- 1..3, into: %{} do
-      {:ok, pid} = Todo.DatabaseWorker.start(@db_folder)
+      {:ok, pid} = Todo.DatabaseWorker.start_link(@db_folder)
       {index - 1, pid}
     end
   end
--- lib/todo/database_worker.ex
+++ lib/todo/database_worker.ex
@@ -1,9 +1,9 @@
 defmodule Todo.DatabaseWorker do
   use GenServer
 
-  def start(db_folder) do
+  def start_link(db_folder) do
     IO.puts("Starting the database worker.")
-    GenServer.start(__MODULE__, db_folder)
+    GenServer.start_link(__MODULE__, db_folder)
   end
 
   def store(worker_pid, key, data) do
