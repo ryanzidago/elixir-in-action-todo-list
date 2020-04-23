93d57029751537ff60d89db80424021d63c48cd6
Ryan Zidago <ryanzidago@gmail.com>
2020-04-23 13:55:11 +0200

    implemented poolboy

--- lib/todo/database_worker.ex
+++ lib/todo/database_worker.ex
@@ -1,21 +1,19 @@
 defmodule Todo.DatabaseWorker do
   use GenServer
 
-  def start_link({db_folder, worker_id}) do
-    IO.puts("Starting the database worker #{worker_id}.")
+  def start_link(db_folder) do
+    IO.puts("Starting the database worker.")
 
-    GenServer.start_link(__MODULE__, db_folder, name: via_tuple(worker_id))
+    GenServer.start_link(__MODULE__, db_folder)
   end
 
   def store(worker_id, key, data) do
-    GenServer.cast(via_tuple(worker_id), {:store, key, data})
+    GenServer.cast(worker_id, {:store, key, data})
   end
 
   def get(worker_id, key) do
-    GenServer.call(via_tuple(worker_id), {:get, key})
+    GenServer.call(worker_id, {:get, key})
   end
-
-  defp via_tuple(worker_id), do: Todo.ProcessRegistry.via_tuple({__MODULE__, worker_id})
 
   @impl GenServer
   def init(db_folder) do
